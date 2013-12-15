33e02667a6cbcde18a965f98ea8dbd22484466f4
Niall Ryan <ryann4@tcd.ie>
2013-12-15 15:34:27 +0000

    Modularised code

--- lib/AuthorInfo.rb
+++ lib/AuthorInfo.rb
@@ -8,30 +8,44 @@ require "json"
 
 module AuthorInfo
 
-	def self.getInfo(name)
-		
-		# format user input => replace spaces with underscores
-		# sub replaces first instance, gsub will replace all => Global SUBstitution
-		query = name.gsub(" ", "_")
+  # format user input => replace spaces with underscores
+  # sub replaces first instance, gsub will replace all => Global SUBstitution
+  def self.format_name(input)
+    return input.gsub(" ", "_")
+  end
 
-		# GET request to Wikipedia
-		uri = URI.parse("http://en.wikipedia.org/w/api.php?format=json&action=parse&page=#{query}")
-		# http://en.wikipedia.org/w/index.php?action=render&title=James_Joyce
-		response = Net::HTTP.get_response(uri)
-		
-		# Manipulate response
-		data = response.body
-		result = JSON.parse(data)
+  def self.make_request(input)
+    # GET request to Wikipedia
+    uri = URI.parse("http://en.wikipedia.org/w/api.php?format=json&action=parse&page=#{input}")
+    # make request
+    response = Net::HTTP.get_response(uri)
+    return response
+  end
 
-		# Write response to file separately as JSON and Ruby Hash
-		# Not necessary, useful to visualise data structure
-		File.open("response.json", 'w') { |file| file.write(result) }
-		File.open("response.rb", 'w') { |file| file.write(result) }
-		
-		# get page HTML
-		content = result["parse"]["text"]["*"]
-		# get only summary
-		content_array = content.split("</p>")
+  def self.convert_response(input)
+    # Manipulate response
+    data = input.body
+    # convert JSON to Ruby Hash
+    result = JSON.parse(data)
+    return result
+  end
+
+  def self.write_response(input)
+    # Write response to file separately as JSON and Ruby Hash
+    # Not necessary, useful to visualise data structure
+    File.open("response.json", 'w') { |file| file.write(input) }
+    File.open("response.rb", 'w') { |file| file.write(input) }
+  end
+
+  def self.get_page(input)
+    # get page HTML
+    content = input["parse"]["text"]["*"]
+    return content
+  end
+
+  def self.get_summary(input)
+    # get only summary
+    content_array = input.split("</p>")
     stripped_content_array = []
     content_array.each do |x|
       # Use Regex to strip out HTML tags => Update this to use Nokogiri or something similar to parse HTML instead
@@ -43,16 +57,34 @@ module AuthorInfo
     end
 
     # concat first two paragraphs of response to create summary
-		summary = stripped_content_array[0].concat(stripped_content_array[1])
-		# write summary to text file
-		File.open("page.txt", 'a') { |file| file.write(summary) }
+    summary = stripped_content_array[0].concat(stripped_content_array[1])
+    # write summary to text file
+    File.open("page.txt", 'a') { |file| file.write(summary) }
+
+    return summary
+
+  end
+
+  def self.getInfo(name)
+
+    query = format_name(name)
+
+    response = make_request(query)
+
+    result = convert_response(response)
+
+    write_response(result)
+
+    content = get_page(result)
+
+    summary = get_summary(content)
 
     puts summary
-		return summary
+    return summary
 
-	end
+  end
 
 end
 
 # for testing module in command line
-# AuthorInfo.getInfo("James Joyce")
+AuthorInfo.getInfo("James Joyce")
